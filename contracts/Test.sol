09c090197ffc510f2ef344f3681f078c5ff9389a
Ororura <hitmanlol890@gmail.com>
2023-09-07 11:19:27 +0300

    fix code

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Test{
+contract Test {
     address public owner;
     enum Role { User, Market }
 
     struct User {
         uint purchased;
         Role role;
-        Product[] product;
     }
 
     struct Product {
@@ -18,44 +17,29 @@ contract Test{
     }
 
     mapping(address => User) public users;
+    mapping(address => Product[]) public userProducts;
 
-    constructor(){
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    constructor() {
         owner = msg.sender;
-        users[owner] = User(0, Role.Market, new Product[](0));
+        users[owner] = User(0, Role.Market);
     }
 
-    function addItems(uint _inStock, uint _price, string calldata _name) public {
-        
+    function addItems(uint _inStock, uint _price, string calldata _name) public onlyOwner {
         Product memory item = Product(_inStock, _price, _name);
-        users[owner].product.push(item);
+        userProducts[owner].push(item);
     }
 
-    function showItems() public view returns(Product[] memory) {
-        return users[owner].product;
+    function showItems() public view returns (Product[] memory) {
+        return userProducts[owner];
     }
 
-    function showInStock() public view returns(uint) {
-        return users[owner].product[0].inStock;
+    function showInStock(uint productId) public view returns (uint) {
+        require(productId < userProducts[owner].length, "Invalid product ID");
+        return userProducts[owner][productId].inStock;
     }
-
-    // function purchaseBread(uint _amount, uint _id) public payable {
-    //     require(_amount <=users[owner].product[_id].inStock); // Покупают меньше, чем есть в наличии
-    //     require(_amount > 0); //Пользователь покупает больше 0
-    //     uint totalPrice = _amount * users[owner].product[_id].price * 1 ether;
-    //     require(msg.value >= totalPrice); // Отправленных денег больше, чем итоговая цена
-        
-    //     Product memory 
-    //     users[owner].product[0].inStock -= _amount;
-
-    //     users[msg.sender].
-    //     users[msg.sender].breadsPurchased += _amount;
-
-    //     if (msg.value > totalPrice) {
-    //         uint change = msg.value - totalPrice;
-    //         payable(msg.sender).transfer(change);
-    //     }
-
-    //     uint balance = address(this).balance;
-    //     payable(owner).transfer(balance);
-    // }
 }
