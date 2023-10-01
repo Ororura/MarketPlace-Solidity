57594875181d2f0a36ba21ab5c24b732c98fc4da
Egor <78803503+Ororura@users.noreply.github.com>
2023-10-01 11:31:49 +0300

    Add registration

--- contracts/edits.sol
+++ contracts/edits.sol
@@ -1,18 +1,20 @@
 // SPDX-License-Identifier: MIT
  
 pragma solidity ^0.8.21;
+/* 
+TODO: 
+1) Пофиксить проблему с id товара при возврате товара (возможно возврате, не помню). Решение: Оставить по дфеолту 0 и при покупки ставить id в магазине. 
+2) Добавить Логин и фио к юзеру
+3) Сделать маппинг из паролей
+4) Попробовать убрать маппинги магазинов, супплаеров и оставить один. (Добавить адресс к продукту) обращаемся к  
+ПРОБЛЕМА:  
+*/
+
 
 contract MarketPlace {
     enum Role { User, Market, Supplier } // Роли пользователей 
     enum Status { Created, Prepairing, Canceled, Complete} // Статусы создания доставки  
 
-    Ticket[] public tickets; // Массив из тикетов на смену ролей. 
-    address public owner; 
-    DeliveryOrder[] public deliveryOrders; // Список заказов на доставку 
-
-    constructor() {
-        owner = msg.sender;
-    }
 
     struct DeliveryOrder { // Структура заказов 
         address userAddr;
@@ -29,11 +31,17 @@ contract MarketPlace {
     }
 
     struct User { // Структура пользователей
+        // добавить логин, фио
+        string login;
+        string firstName;
+        string lastName;
         Role role;
         uint balance;
     }
 
     struct Product { // Структура продукта
+        uint id;
+        // shopAddress
         address userAddress;
         uint inStock;
         uint price;
@@ -41,12 +49,26 @@ contract MarketPlace {
         uint expDate;
     }
 
+    Ticket[] public tickets; // Массив из тикетов на смену ролей. 
+    address public owner; 
+    DeliveryOrder[] public deliveryOrders; // Список заказов на доставку 
 
     mapping(address => User) public users;  // Маппинг всех пользователей 
+
+    // 1 маппинг, чтобы достать продукты каждого юзера
+
     mapping(address => Product[]) public userProducts; // Инвентарь продуктов обычного покупателя
+
     mapping(address => Product[]) public marketProducts; // Инвентарь продуктов магазина 
     mapping(address => Product[]) public supplierProducts; // Инвентарь поставщиков
+
     mapping(string => address) public referrals; // Рефералка привязанная к пользователю, которого указали при создании
+    // хэшированный пароль
+    mapping(address => bytes32) public passwords;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
 
     modifier OnlyOwner(){
@@ -58,18 +80,38 @@ contract MarketPlace {
         require(users[msg.sender].role == _role, "Your role does not allow this change");
         _;
     }
-
-    function makeUser() external { // Создание покупателя
-        users[msg.sender] = User(Role.User, 0);
+    // !!!!!!!! Привязан ли адресс к аккаутну пользователя? 
+    function registration(string memory _login, string memory _firstName, string memory _lastName, string memory _password) external {
+        users[msg.sender] = User(_login, _firstName, _lastName, Role.User, 0);
+        passwords[msg.sender] = keccak256(abi.encode(_password));
     }
 
-    function makeSupplier() external  { // Создание поставщика
-        users[msg.sender] = User(Role.Supplier, 0);
+    function login(string memory _login, string memory _password) external view returns(User memory) {
+        string memory storedLogin = users[msg.sender].login;
+        require(keccak256(abi.encodePacked(_login)) == keccak256(abi.encodePacked(storedLogin)));
+        require(keccak256(abi.encode(_password)) == passwords[msg.sender]);
+        return users[msg.sender];
     }
 
-    function makeMarket() external   { // Создание магазина 
-        users[msg.sender] = User(Role.Market, 0);
+    // регистрация, проверка на существующего пользователя по адресу, по логину
+
+    // проверка совпали ли логин пароль, возвращается структура
+
+    // function makeUser() external { // Создание покупателя
+    //     users[msg.sender] = User(Role.User, 0);
+    // }
+
+    function getProducts(address _shop) public view returns(Product[] memory){
+        return marketProducts[_shop];
     }
+
+    // function makeSupplier() external  { // Создание поставщика
+    //     users[msg.sender] = User(Role.Supplier, 0);
+    // }
+
+    // function makeMarket() external   { // Создание магазина 
+    //     users[msg.sender] = User(Role.Market, 0);
+    // }
 
     function approveChangeRole(uint _idTicket) external  OnlyOwner { // Подтверждение смены роли для пользователя ( Подтверждать смену может только ВЛАДЕЛЦ контракта). Передается id тикета
         address userAddr = tickets[_idTicket].userAddr;
@@ -77,7 +119,8 @@ contract MarketPlace {
         users[userAddr].role = changedRole;
     }
 
-    function changeRole(Role _role) public { // Создание тикета на смену роли. Указываем роль, которую хотим сменить 
+    function changeRole(Role _role) public { // Создание тикета на смену роли. Указываем роль, которую хотим сменить
+    // сделать проверку, чтобы человек не мог отправить 2 раза тикет 
         tickets.push(Ticket(msg.sender, _role));
     }
 
@@ -131,12 +174,12 @@ contract MarketPlace {
     }
 
     function addItemsSupplier(uint _inStock, uint _price, string calldata _name, uint _expDate, address _addressSupp) external  AccessControl(Role.Supplier) { // Добавить товары для поставщика
-        Product memory item = Product(_addressSupp, _inStock, _price / 2, _name, _expDate);
+        Product memory item = Product(0, _addressSupp, _inStock, _price / 2, _name, _expDate);
         supplierProducts[_addressSupp].push(item);
     }
 
     function addItemsMarket(uint _inStock, uint _price, string calldata _name, uint _expDate) external  AccessControl(Role.Market) { // Добавить товары для магазина (По логике, мы пополняем товары через поставщика. Я добавил эту функцию, чтобы не тратить время во время тестирования)
-        Product memory item = Product(msg.sender, _inStock, _price, _name, _expDate);
+        Product memory item = Product(0, msg.sender, _inStock, _price, _name, _expDate);
         marketProducts[msg.sender].push(item);
     }
     
@@ -159,7 +202,7 @@ contract MarketPlace {
         }
 
         if (!productExists){
-            Product memory newItem = Product(_supplier, _amount, price, targetName, supplierProducts[_supplier][_productId].expDate);
+            Product memory newItem = Product(0, _supplier, _amount, price, targetName, supplierProducts[_supplier][_productId].expDate);
             marketProducts[_shop].push(newItem);
         }
 
@@ -171,9 +214,10 @@ contract MarketPlace {
 
     }
 
+    
 
-
-    function refund(address _shop, uint _productId) external  { // Сделать возврат товара. Указываем магазин и id возвращаемого товра. 
+    // Проблема: как нам перебирать mapping
+    function refundRequest(address _shop, uint _productId) external  { // Сделать возврат товара. Указываем магазин и id возвращаемого товра. 
         require(userProducts[msg.sender][_productId].expDate > marketProducts[_shop][_productId].expDate);
         uint totalRefSum =  userProducts[msg.sender][_productId].inStock * marketProducts[_shop][_productId].price;
         marketProducts[_shop][_productId].inStock += userProducts[msg.sender][_productId].inStock;
@@ -182,17 +226,17 @@ contract MarketPlace {
         payable(msg.sender).transfer(totalRefSum);
     }
 
+
     function purchase(address _shop, uint _amount, uint _id, string memory _ref ) public payable { // Покупаем со стороны пользователя у магазина продукты. Указываем магазин, id продукта, кол-во, Рефералку(может использовать только тот, которого указали при создании рефералки)
-        if (users[msg.sender].role == Role.User) {
-            users[msg.sender] = User( Role.User, 0);
-        }
+        // if (users[msg.sender].role == Role.User) {
+        //     users[msg.sender] = User( Role.User, 0);
+        // }
         
         require(_amount > 0, "Purchase amount must be greater than 0");
         require(_id < marketProducts[_shop].length, "Invalid product ID");
         require(_amount <= marketProducts[_shop][_id].inStock, "Not enough stock available");
-
+        
         uint totalPrice = _amount * marketProducts[_shop][_id].price;
-
         if (referrals[_ref] == msg.sender) {
             totalPrice = (totalPrice * 90 / 100);
             referrals[_ref] = address(0);
@@ -210,12 +254,11 @@ contract MarketPlace {
         }
 
         if (!productExists) {
-            Product memory newItem = Product(msg.sender, _amount, marketProducts[_shop][_id].price, marketProducts[_shop][_id].name, randMod(1000));
-            userProducts[msg.sender].push(newItem);
+            userProducts[msg.sender].push(Product(_id, _shop, _amount, msg.value, marketProducts[_shop][_id].name, randMod(1000)));
         }
 
         marketProducts[_shop][_id].inStock -= _amount;
-        users[_shop].balance += totalPrice;
+        users[_shop].balance += totalPrice; // -> msg.value
 
     }
 
